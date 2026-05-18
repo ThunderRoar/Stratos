17dfbacff040fc09b4b91b1043c702262bb74d30
Shreyas Rao <raoshreyas2004@gmail.com>
2026-05-17 22:05:09 -0400

    add scaffold with dapp kit providers and required contract info

---
+++ frontend/src/config/constants.ts
@@ -0,0 +1,11 @@
+// If the constants change, refer to the documentation https://docs.sui.io/onchain-finance/deepbook-predict/contract-information
+
+export const NETWORK = 'testnet' as const
+export const PREDICT_SERVER_URL = 'https://predict-server.testnet.mystenlabs.com'
+export const PREDICT_PACKAGE_ID = '0xf5ea2b3749c65d6e56507cc35388719aadb28f9cab873696a2f8687f5c785138'
+export const PREDICT_REGISTRY_ID = '0x43af14fed5480c20ff77e2263d5f794c35b9fab7e2212903127062f4fe2a6e64'
+export const PREDICT_OBJECT_ID = '0xc8736204d12f0a7277c86388a68bf8a194b0a14c5538ad13f22cbd8e2a38028a'
+export const QUOTE_ASSET_TYPE = '0xe95040085976bfd54a1a07225cd46c8a2b4e8e2b6732f140a0fc49850ba73e1a::dusdc::DUSDC'
+export const DUSDC_CURRENCY_ID = '0xf3000dff421833d4bb8ed58fac146d691a3aaba2785aa1989af65a7089ca3e9c'
+export const PLP_COIN_TYPE = '0xf5ea2b3749c65d6e56507cc35388719aadb28f9cab873696a2f8687f5c785138::plp::PLP'
+export const DUSDC_DECIMALS = 6
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { DAppKitProvider } from '@mysten/dapp-kit-react'
+import { createDAppKit } from '@mysten/dapp-kit-core'
+import { SuiJsonRpcClient, getJsonRpcFullnodeUrl } from '@mysten/sui/jsonRpc'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const dAppKit = createDAppKit({
+  networks: ['testnet'],
+  defaultNetwork: 'testnet',
+  createClient: (network) =>
+    new SuiJsonRpcClient({
+      url: getJsonRpcFullnodeUrl(network),
+      network,
+    }),
+})
+
+declare module '@mysten/dapp-kit-core' {
+  interface Register {
+    dAppKit: typeof dAppKit
+  }
+}
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <DAppKitProvider dAppKit={dAppKit}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </DAppKitProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
 )
